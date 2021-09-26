b2c08b0b58ad085e74e7443477c20e99809665e8
hiro-lapis <coolish555ddddd@gmail.com>
2021-09-27 00:47:58 +0900

    unitテストに異常系テスト追加

--- tests/Unit/Requests/Products/StoreRequestTest.php
+++ tests/Unit/Requests/Products/StoreRequestTest.php
@@ -7,7 +7,6 @@ use Tests\TestCase;
 // use PHPUnit\Framework\TestCase;
 use App\Http\Requests\Products\StoreRequest;
 use App\Models\Category;
-use Database\Seeders\CategorySeeder;
 use Illuminate\Support\Facades\Validator;
 
 /**
@@ -24,8 +23,7 @@ class StoreRequestTest extends TestCase
      */
     public function test_正常値入力時にパスするか(array $data):void
     {
-        $category_id = Category::factory()->create()->id;
-        $data['category_id'] = $category_id;
+        $data['category_id'] = Category::factory()->create()->id;
 
         // バリデーションルールを抽出
         $rules = (new StoreRequest())->rules();
@@ -52,7 +50,81 @@ class StoreRequestTest extends TestCase
                     'price' => 100,
                     'stock' => 1,
                 ],
-            ]
+            ],
+            '#1' => [
+                [
+                    'name' => '商品名',
+                    'price' => 100,
+                    'stock' => 1,
+                ],
+            ],
         ];
+    }
+
+    /**
+     * 異常値入力データプロバイダ
+     *
+     * @return array
+     */
+    public function storeInvalidRequestDataProvider():array
+    {
+        return [
+            '#1 商品名未入力' => [
+                [
+                    'name' => null,
+                    'price' => 100,
+                    'stock' => 1,
+                ]
+            ],
+            '#2 最低価格未満' => [
+                [
+                    'name' => '商品名',
+                    'price' => 0,
+                    'stock' => 1,
+                ]
+            ],
+            '#3 最高価格オーバー' => [
+                [
+                    'name' => '商品名',
+                    'price' => 100000,
+                    'stock' => 1,
+                ]
+            ],
+            '#4 カテゴリー未存在' => [
+                [
+                    'name' => '商品名',
+                    'category_id' => 999999,
+                    'price' => 100,
+                    'stock' => 0,
+                ],
+            ],
+            '#5 最高在庫数オーバー' => [
+                [
+                    'name' => '商品名',
+                    'price' => 100,
+                    'stock' => 1000,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @dataProvider storeInvalidRequestDataProvider
+     * @return void
+     */
+    public function test_異常値入力時にエラーメッセージを返すか(array $data):void
+    {
+        $data['category_id'] = isset($data['category_id']) ? $data['category_id'] : Category::factory()->create()->id;
+
+        // バリデーションルールを抽出
+        $rules = (new StoreRequest())->rules();
+        // 入力値と合わせてバリデータ作成
+        $validator = Validator::make($data, $rules);
+        // バリデーション実行
+        $result = $validator->passes();
+        // 期待値の判定
+        $this->assertFalse($result);
     }
 }
